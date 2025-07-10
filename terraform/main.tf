bca54cc1859242c0832de2db25e351bd4f1ec1dc
guiaf04 <guilhermearaujo@alu.ufc.br>
2025-07-10 16:34:54 -0300

    Update AWS AMI filter to support all Ubuntu 22.04 versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,10 +40,10 @@ data "aws_availability_zones" "available" {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical
-  
+
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-22.04-amd64-server-*"]
   }
   
   filter {
